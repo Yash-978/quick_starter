288c7b385d0ed5edf5c91d8eb3f079a7ffe17acb
Yash-978 <virwalyash@gmail.com>
2024-03-24 03:19:14 +0530

    23/3/24

---
+++ lib/R&W vertical design.dart
@@ -0,0 +1,234 @@
+// Red & White in vertical design
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '',
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.red,
+          title: const Text(
+            'Red & White',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        body: const Center(
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: '\t\t\t\t\t\t\t\t\tG',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+
+                  ),
+                ),
+                TextSpan(
+                  text: 'R',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'APHICS\n',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'FLUTT',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'E',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'R\n',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: '\t\t\t\t\t\tAN',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'D',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'ROID\n',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'DESIGN',
+                  style: TextStyle(
+                    color: Colors.orange,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: ' & ',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'DEVELOP\n',
+                  style: TextStyle(
+                    color: Colors.orange,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: '\t\t\t\t\t\t\t  W',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'EB\n',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: '\t\t\t FAS',
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'H',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'ION\n',
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'ANIMAT',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'I',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'ON\n',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: '\t\t\t\t\t\t\t\t\t\t\tI',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'T',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'A-CS+\n',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: '\t\t\t\t\tGAM',
+                  style: TextStyle(
+                    color: Colors.orange,
+                    fontSize: 29,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextSpan(
+                  text: 'E',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+    ),
+  );
+}
